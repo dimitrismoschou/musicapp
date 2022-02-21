584128447f4e7e4dd613f2645c9ac74a394314d0
Demetrius Moschou <47526339+dimitrismoschou@users.noreply.github.com>
2022-02-21 22:39:03 +0200

    Changed main menu

--- app/src/main/java/com/unipi/mosdim/musicapp/MainActivity.java
+++ app/src/main/java/com/unipi/mosdim/musicapp/MainActivity.java
@@ -1,18 +1,29 @@
 package com.unipi.mosdim.musicapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ScrollView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     Button btnLogOut;
     FirebaseAuth mAuth;
@@ -35,7 +46,37 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
         });
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
 
+        setSupportActionBar(toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close )
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_home);
+
+    }
+
+    /**
+     *
+     * When clicking to return with MENU open, MENU is only collapsed.
+     *
+     * */
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     protected void onStart () {
@@ -44,5 +85,19 @@ public class MainActivity extends AppCompatActivity {
         if (user == null) {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.nav_home:
+                break;
+            case R.id.nav_pag1:
+                Toast.makeText(this, "Pag1", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return false;
     }
 }
