c3d7b0e3c9db571f8dd662ddc25f43fef01c8fb1
vanglovis <vagelisfitros@gmail.com>
2022-02-22 11:39:02 +0200

    moving textview, set textview timers

--- app/src/main/java/com/unipi/mosdim/musicapp/MainActivity.java
+++ app/src/main/java/com/unipi/mosdim/musicapp/MainActivity.java
@@ -1,16 +1,20 @@
 package com.unipi.mosdim.musicapp;
 
 import static android.view.Gravity.CENTER;
+import static android.view.Gravity.FILL_HORIZONTAL;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -20,6 +24,7 @@ import android.widget.NumberPicker;
 import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -30,29 +35,39 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
     SeekBar seekBar;
     Button btnLogOut;
     FirebaseAuth mAuth;
     LinearLayout layout,l, firstlayout;
     ScrollView scrollView;
     MediaPlayer mediaPlayer = new MediaPlayer();
-    int length=0;
+    TextView movingText,minText,maxText;
     ArrayList<String> songName = new ArrayList<>();
     ArrayList<String> artistName = new ArrayList<>();
     ArrayList<String> category = new ArrayList<>();
     ArrayList<String> link = new ArrayList<>();
     ArrayList<String> location = new ArrayList<>();
+    String getLink="";
+    int length=0;
+
+    private Handler mSeekbarUpdateHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         seekBar = (SeekBar) findViewById(R.id.seekBar3);
-
-
+        minText = findViewById(R.id.minTime);
+        maxText = findViewById(R.id.maxTime);
+        movingText = findViewById(R.id.movingText);
+        movingText.setSelected(true);
         btnLogOut = findViewById(R.id.btnLogout);
         mAuth = FirebaseAuth.getInstance();
 
@@ -61,17 +76,18 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
         });
         DatabaseReference myRef;
-        myRef = FirebaseDatabase.getInstance("https://musicapp-ad62e-default-rtdb.firebaseio.com/").getReference();
+        myRef = FirebaseDatabase.getInstance("https://musicapp-ad62e-default-rtdb.firebaseio.com/").getReference().child("songs");
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 int i=0;
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    songName.add(snapshot.getKey());   //τιτλος βιβλιων
+                    songName.add((String) snapshot.child("name").getValue());   //τιτλος βιβλιων
                     artistName.add((String) snapshot.child("artist").getValue());
                     link.add((String) snapshot.child("link").getValue());
                     location.add((String) snapshot.child("location").getValue());
                     category.add((String) snapshot.child("category").getValue());
+
                     getAllData(i);
                     i++;
                 }
@@ -79,7 +95,26 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-            }});
+            }
+        });
+
+//        DatabaseReference myRef2 = FirebaseDatabase.getInstance("https://musicapp-ad62e-default-rtdb.firebaseio.com/").getReference().child("user_pref");
+//        myRef2.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+//                    if (!((String)snapshot.child("genre").getValue()).equals("rock")){
+//                        Toast.makeText(MainActivity.this, "Rock", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -99,61 +134,79 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
     }
-    private Handler mSeekbarUpdateHandler = new Handler();
+
     private Runnable mUpdateSeekbar = new Runnable() {
         @Override
         public void run() {
             seekBar.setProgress(mediaPlayer.getCurrentPosition());
             mSeekbarUpdateHandler.postDelayed(this, 50);
+            setTime();
         }
     };
+
+    public void setTime(){
+        String time =(new SimpleDateFormat("m:ss")).format(new Date(mediaPlayer.getCurrentPosition()));
+        minText.setText(time);
+    }
+
     public void getAllData(int i){
         System.out.println(songName);
+
         //hardcoded components
         layout = findViewById(R.id.layout_parent);
         scrollView = findViewById(R.id.scrollView_parent);
 
-        firstlayout = new LinearLayout(this);
-        firstlayout.setOrientation(LinearLayout.HORIZONTAL);
-        LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        LinearLayout.LayoutParams lparams_inside = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
 
         TextView t1 = new TextView(this);
-        t1.setLayoutParams(lparams);
-        t1.setText(songName.get(i));
+        //t1.setLayoutParams(lparams);
+        t1.setText(artistName.get(i));
 
         LinearLayout l1h = new LinearLayout(this);
+        l1h.setWeightSum(1);    //controls the weights in l1v and playbtn
         l1h.setOrientation(LinearLayout.HORIZONTAL);
-        l1h.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        l1h.setGravity(CENTER);
+        l1h.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
 
         LinearLayout l1v = new LinearLayout(this);
         l1v.setOrientation(LinearLayout.VERTICAL);
+        lparams_inside.weight = 9;
+        l1v.setLayoutParams(lparams_inside);
 
         TextView title1 = new TextView(this);
-        title1.setLayoutParams(lparams);
-        title1.setText(category.get(i));
+        //title1.setLayoutParams(lparams);
+        title1.setText(songName.get(i));
         l1v.addView(title1);
         l1v.addView(t1);
 
         Button playbtn = new Button(this);
-        playbtn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        lparams_inside.weight = 1;
+        playbtn.setLayoutParams(lparams_inside);
         playbtn.setText("Play");
         playbtn.setId(i);
         playbtn.setEnabled(true);
         playbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!mediaPlayer.isPlaying() && length==0) {
+                if(!mediaPlayer.isPlaying() && !getLink.equals(link.get(i))) {
+                    mediaPlayer.reset();
                     try {
                         mediaPlayer.setDataSource(link.get(i));
                         mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                            @RequiresApi(api = Build.VERSION_CODES.M)
                             @Override
                             public void onPrepared(MediaPlayer mediaPlayer) {
+                                getLink = link.get(i);
+                                maxText.setText((new SimpleDateFormat("m:ss")).format(new Date(mediaPlayer.getDuration())));
                                 seekBar.setMax(mediaPlayer.getDuration());
                                 mediaPlayer.start();
                                 playbtn.setText("Pause");
                                 mSeekbarUpdateHandler.postDelayed(mUpdateSeekbar, 0);
+                                mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(1));
+                                movingText.setText(songName.get(i)+","+artistName.get(i));
                             }
                         });
                         mediaPlayer.prepare();
@@ -178,11 +231,11 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        l1h.addView(playbtn);
         l1h.addView(l1v);
+        l1h.addView(playbtn);
 
-        firstlayout.addView(l1h);
-        this.layout.addView(firstlayout);
+        //firstlayout.addView(l1h);
+        this.layout.addView(l1h);
     }
 
 //    public void playmusic(View view){
@@ -214,9 +267,6 @@ public class MainActivity extends AppCompatActivity {
 //        mediaPlayer.seekTo(length);
 //        mediaPlayer.start();
 //    }
-
-
-
 
     protected void onStart () {
         super.onStart();
