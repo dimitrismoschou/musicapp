729ed3ebea81e64bef93ea2c4fd7feaede62bd26
a.petridis <axilleas.petr@gmail.com>
2022-03-05 19:44:09 +0200

    Settings activity + Logout

--- app/src/main/java/com/unipi/mosdim/musicapp/SettingsActivity.java
+++ app/src/main/java/com/unipi/mosdim/musicapp/SettingsActivity.java
@@ -4,20 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsActivity extends AppCompatActivity {
 
     Button buttonLogout;
-
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
+        mAuth = FirebaseAuth.getInstance();
         buttonLogout = findViewById(R.id.buttonLogout);
+
+    }
+    public void logout(View v){
         mAuth.signOut();
-        startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        mediaPlayer.reset();
+        startActivity(new Intent(SettingsActivity.this, LoginActivity.class));
+        MainActivity.mediaPlayer.reset();
         finish();
     }
 }
--- app/src/main/java/com/unipi/mosdim/musicapp/UserProfile.java
+++ app/src/main/java/com/unipi/mosdim/musicapp/UserProfile.java
@@ -3,24 +3,19 @@ package com.unipi.mosdim.musicapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Objects;
 
 public class UserProfile extends AppCompatActivity {
     TextInputEditText edit_email;
